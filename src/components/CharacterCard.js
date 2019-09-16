ad11433a02b2cae4cad5d7d4973f17fb6f7eb314
josepaniagua <jpanjr@gmail.com>
2019-09-16 09:37:47 -0700

    charactercard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const CardExampleCardProps = (props) => (
+    <Card
+      image={props.img}
+      header={props.name}
+      meta={props.species}
+      description={props.created}
+    />
+)
+
+export default CardExampleCardProps;
+
+
+// export default function CharacterCard() {
+//   return <span>todo: character</span>;
+// }
